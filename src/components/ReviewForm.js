c48fe807abd81ffb670fbf3ec3bd51c6c63853b1
github username <github email address>
2018-12-29 10:36:54 -0500

    can get id of reviewed book

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -13,7 +13,15 @@ class ReviewForm extends Component {
     this.setState({ [e.target.id]: e.target.value })
   }
 
+  handleBookChange = (e) => {
+    let reviewedBook = e.target.value
+    let foundBook = this.props.allBooks.filter(book => book.title === reviewedBook)
+    return foundBook[0].id
+  }
+
   handleSubmit = (e) => {
+    debugger
+    const bookId = e.currentTarget.children[0].value;
     e.preventDefault();
     fetch('http://localhost:3001/reviews', {
       method: "POST",
@@ -23,16 +31,15 @@ class ReviewForm extends Component {
       },
       body: JSON.stringify({
         details: this.state.reviewDetails,
- 		date: 'jan',
-		reader_id: 1,
-		book_id: 1
+     		date: 'jan',
+    		reader_id: 1,
+    		book_id: bookId
       })
     }).then(res => res.json())
     .then(newRev => console.log(newRev))
   }
 
   render() {
-    console.log(this.state.bookTitle)
     return (
       <form className="ui form" onSubmit={this.handleSubmit}>
         <div className="field">
@@ -41,11 +48,11 @@ class ReviewForm extends Component {
         </div>
         <div className='field'>
           <label>Book Title</label>
-          <input type="text" id='bookTitle' placeholder="Book Title" value={this.state.bookTitle} onChange={this.handleChange}/>
+          <input type="text" id='bookTitle' placeholder="Book Title" value={(e) => this.handleBookChange(e)} onChange={(e) => this.handleBookChange(e)}/>
         </div>
         <div className="field">
           <label>Username</label>
-          <input type="text" id="username" placeholder="Username" value={this.state.username} onChange={this.handleChange}/>
+          <input type="text" id="username" placeholder="Username" onChange={this.handleChange}/>
         </div>
           <button className="ui button" type="submit">Submit</button>
       </form>
